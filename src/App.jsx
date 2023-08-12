d1e6d1299f634696f5029c22384346f45f4dbc50
Joy Sarkar <joysarkar8171@gmail.com>
2023-08-12 23:03:31 +0530

    Repo created and tailwind css added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex text-green-600 font-bold justify-center text-6xl '>HACKAUT</div>
+    </>
+  )
+}
+
+export default App
