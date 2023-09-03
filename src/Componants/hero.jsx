c2cd9955bcd5a29df92eb3d20768b2ca4e21077c
Arnab Pal <arnabpalofficial2003@gmail.com>
2023-09-03 20:07:48 +0530

    fix: updated style

--- src/Componants/hero.jsx
+++ src/Componants/hero.jsx
@@ -3,10 +3,10 @@ import Picture from "../assets/341099819_244778971259675_7302734960212248906_n.j
 
 export const Hero = () => {
   return (
-    <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0 pattern-cross-dots-lg bg-white text-blue-100">
+    <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0  bg-gradient-to-r from-blue-50 text-blue-100">
       <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
         <svg
-          className="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
+          className="absolute left-0 hidden h-full text-blue-50 transform -translate-x-1/2 lg:block"
           viewBox="0 0 100 100"
           fill="currentColor"
           preserveAspectRatio="none slice"
