0921936ec81f3120e205c0323ea32a74eb069705
Arnab Pal <arnabpalofficial2003@gmail.com>
2023-09-06 11:12:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import NavBar2 from "./Componants/navBar2";
 import { Hero } from "./Componants/hero";
 import Form from "./Componants/Form";
-import { Objectives } from "./Componants/objectives";
 
 function App() {
   const [nav, setNav] = useState(false);
@@ -12,7 +11,6 @@ function App() {
     <>
       <NavBar2 nav={nav} setNav={setNav}></NavBar2>
       <Hero/>
-      <Objectives/>
       <Form/>
     </>
   );
