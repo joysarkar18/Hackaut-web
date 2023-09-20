b04f5f1f963e6c3853a9aedfc307807596e61751
Arnab Pal <arnabpalofficial2003@gmail.com>
2023-09-20 21:15:40 +0530

    update react-router-dom

--- src/Componants/Error.jsx
+++ src/Componants/Error.jsx
@@ -1,34 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Error = () => {
   return (
     <section class="page_404 font-mono font-bold">
-	<div class="container1">
-		<div class="row">	
-		<div class="col-sm-12 ">
-		<div class="col-sm-10 col-sm-offset-1  text-center">
-		<div class="four_zero_four_bg">
-			<h1 class="text-center ">404</h1>
-		
-		
-		</div>
-		
-		<div class="contant_box_404">
-		<h3 class="h2">
-		Look like you're lost
-		</h3>
-		
-		<p>the page you are looking for not avaible!</p>
-		
-		<a href="/" class="link_404 rounded">Go to Home</a>
-	</div>
-		</div>
-		</div>
-		</div>
-	</div>
-</section>
-  )
-}
+      <div class="container1">
+        <div class="row">
+          <div class="col-sm-12 ">
+            <div class="col-sm-10 col-sm-offset-1  text-center">
+              <div class="four_zero_four_bg">
+                <h1 class="text-center ">404</h1>
+              </div>
 
-export default Error
+              <div class="contant_box_404">
+                <h3 class="h2">Look like you're lost</h3>
 
+                <p>the page you are looking for not avaible!</p>
+
+                <a href="/" class="link_404 rounded">
+                  Go to Home
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Error;
